61d4129103b7f37ebf18244cb26de086b55a9a20
Swizzy <swizzy@xeupd.com>
2015-07-18 11:19:55 +0200

    Added initial project

---
+++ xeBuild GUI/xeBuild GUI/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿//
+// App.xaml.cs
+// xeBuild GUI
+//
+// Created by Swizzy 18/07/2015
+// Copyright (c) 2015 Swizzy. All rights reserved.
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace xeBuild_GUI {
+
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App {
+        private static readonly Icon Icon =
+            Icon.ExtractAssociatedIcon(
+                                       Path.Combine(
+                                                    Path.GetDirectoryName(Assembly.GetAssembly(typeof(App)).Location),
+                                                    Path.GetFileName(Assembly.GetAssembly(typeof(App)).Location)));
+
+        internal static readonly ImageSource WpfIcon = Imaging.CreateBitmapSourceFromHIcon(
+                                                                                           Icon.Handle, Int32Rect.Empty,
+                                                                                           BitmapSizeOptions
+                                                                                               .FromEmptyOptions());
+
+        internal static Version Version = Assembly.GetAssembly(typeof(App)).GetName().Version;
+        private void AppStart(object sender, StartupEventArgs e) { new MainWindow(e.Args).Show(); }
+    }
+
+}
---
+++ xeBuild GUI/xeBuild GUI/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿// 
+// MainWindow.xaml.cs
+// xeBuild GUI
+// 
+// Created by Swizzy 18/07/2015
+// Copyright (c) 2015 Swizzy. All rights reserved.
+
+namespace xeBuild_GUI {
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow(string[] args) {
+            InitializeComponent();
+            Title = string.Format(Title, App.Version.Major, App.Version.Minor, App.Version.Build);
+            Icon = App.WpfIcon;
+        }
+    }
+
+}
